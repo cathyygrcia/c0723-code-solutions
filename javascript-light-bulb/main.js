812668be55c92bdc6663a5b0008edfc87a171496
Cathy Garcia <garciacathy67@yahoo.com>
2023-07-25 21:35:34 +0000

    Updated javascript-light-bulb exercise.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,15 @@
-const lightBulb = document.querySelector('.bulb');
-const bodyColor = document.querySelector('body');
+const $lightBulb = document.querySelector('.bulb');
+const $bodyColor = document.querySelector('body');
 
 let isBulbOn = true;
 
-lightBulb.addEventListener('click', function (event) {
+$lightBulb.addEventListener('click', function (event) {
   isBulbOn = !isBulbOn;
   if (isBulbOn) {
-    lightBulb.className = 'bulb off';
-    bodyColor.className = 'body dark';
+    $lightBulb.className = 'bulb off';
+    $bodyColor.className = 'body dark';
   } else {
-    lightBulb.className = 'bulb on';
-    bodyColor.className = 'body';
+    $lightBulb.className = 'bulb on';
+    $bodyColor.className = 'body';
   }
 });
